a1e50c38003ccd3ac4e4a4be49b1f7f421d93821
FettesTeil\Shadow <m.gerstl@gmail.com>
2018-06-15 01:48:29 +0200

    increase version number

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <?php
-echo "V1.0.0 last build date: ".date ("Y-m-d H:i:s", filemtime('index.php'))."<br>";
+echo "V1.0.1 last build date: ".date ("Y-m-d H:i:s", filemtime('index.php'))."<br>";
 
 echo '
 <a href="index.php" tooltip title="HOME" alt="HOME">
