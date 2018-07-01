720dd747b2d13f3c0e09c6381dda37e1decd1bf3
FettesTeil\Shadow <m.gerstl@gmail.com>
2018-07-01 18:35:54 +0200

    update

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <?php
-echo "V2.0.0 last build date: ".date ("Y-m-d H:i:s", filemtime('menu.php'))."<br>";
+echo "V2.0.1 last build date: ".date ("Y-m-d H:i:s", filemtime('menu.php'))."<br>";
 
 echo '
 <a href="index.php" tooltip title="HOME" alt="HOME">
