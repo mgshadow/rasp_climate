b068fedaf127ce580cfe722d55ecad17e6c551d0
mgshadow <39982543+mgshadow@users.noreply.github.com>
2018-11-25 00:48:47 +0100

    Update dataProvider.php

--- dataProvider.php
+++ dataProvider.php
@@ -1,4 +1,15 @@
 <?php 
+include("./include/sensorclass.php");
+include("./include/drawerclass.php");
+
+
+$inSensor=SensorFactory::getInsideSensor();
+$outSensor=SensorFactory::getOutsideSensor();
+$box1Sensor=SensorFactory::getBox1Sensor();
+$box2Sensor=SensorFactory::getBox2Sensor();
+$sensors=array($outSensor,$inSensor, $box1Sensor,$box2Sensor);
+
+
  header("Access-Control-Allow-Origin: *");
  header("Content-Type: application/json");
 
@@ -9,21 +20,22 @@ if ($_GET['duration']!=null)
 $db = mysqli_connect("localhost","datalogger","datalogger") or die("DB Connect error"); 
 mysqli_select_db($db, "datalogger");
 $sql  = "SELECT min(m.date_time), ";
-$sql=$sql."round(avg(t0.temperature),1) as tt0, round(avg(t0.humidity),1) as th0, ";
-$sql=$sql."round(avg(t1.temperature),1) as tt1, round(avg(t1.humidity),1) as th1, ";
-$sql=$sql."round(avg(t2.temperature),1) as tt2, round(avg(t2.humidity),1) as th2, ";
-$sql=$sql."round(avg(t3.temperature),1) as tt3, round(avg(t3.humidity),1) as th3 ";
-$sql=$sql."FROM measure m left join datalogger as t0 on t0.sensor=0 and t0.measureid=m.id left join datalogger as t1 on t1.sensor=1 and t1.measureid=m.id left join datalogger as t2 on t2.sensor=2 and t2.measureid=m.id left join datalogger as t3 on t3.sensor=3 and t3.measureid=m.id ";
+for ($sensorIndex=0;$sensorIndex<count($sensors);$sensorIndex++)
+	{
+	$p=$sensors[$sensorIndex]->pin;
+	$sql=$sql."round(avg(t$p.temperature),1) as tt$p, round(avg(t$p.humidity),1) as th$p, ";
+	}
+$sql=$sql."FROM measure m ";
+for ($sensorIndex=0;$sensorIndex<count($sensors);$sensorIndex++)
+	{
+	$p=$sensors[$sensorIndex]->pin;
+	$sql=$sql."left join datalogger as t$p on t$p.sensor=$p and t$p.measureid=m.id ";
+	}
 $sql=$sql."WHERE m.active=1 and TIMESTAMPDIFF(HOUR,m.date_time,NOW())<$duration GROUP BY m.date_time order by m.date_time";
 $result = mysqli_query($db, $sql);
 $first=1;
-?>
-{
-"querystringDuration": "<?php echo($_GET['duration']); ?>",
-"duration": <?php echo($duration); ?>,
-"data":
-[
-<?php
+
+$data=array();
 while($r = mysqli_fetch_array($result))
 {
   $line='';
@@ -31,22 +43,43 @@ while($r = mysqli_fetch_array($result))
 	$line=$line.',';	  
 
   $first=0;
-  $line=$line.'{';
-  $line=$line.'"date": {"years": '.substr($r[0],0,4).', "months": '.(((int)substr($r[0],5,2))-1).', "days": '.((int)substr($r[0],8,2)).', "hours":'.((int)substr($r[0],11,2)).',"minutes": '.((int)substr($r[0],14,2)).',"seconds":'.((int)substr($r[0],17,2)).'}, ';
+  $arrDate = array('years' => substr($r[0],0,4), 
+		   'months' => (((int)substr($r[0],5,2))-1), 
+		   'days' => (((int)substr($r[0],8,2))), 
+		   'hours' => ((int)substr($r[0],11,2)), 
+		   'minutes' => ((int)substr($r[0],14,2)),
+		   'seconds' => ((int)substr($r[0],17,2)));
+  $arrSensors=array();
+  for ($sensorIndex=0;$sensorIndex<count($sensors);$sensorIndex++)
+	{
+	$name=$sensors[$sensorIndex]->name;
+	$temperature=$r[$sensorIndex+1]==null?"null":$r[$sensorIndex+1];
+	$humidity=$r[$sensorIndex+2]==null?"null":$r[$sensorIndex+2];
+	$arrSensors[]=array('name' => $name,
+			    'temperature' => $temperature,
+			    'humidity' => $humidity,);
+  	}
+  $data[]=array('date' => $arrDate,
+		'sensors' => $arrSensors);
+  #$line=$line.'{';
+  #$line=$line.'"date": {"years": '.substr($r[0],0,4).', "months": '.(((int)substr($r[0],5,2))-1).', "days": '.((int)substr($r[0],8,2)).', "hours":'.((int)substr($r[0],11,2)).',"minutes": '.((int)substr($r[0],14,2)).',"seconds":'.((int)substr($r[0],17,2)).'}, ';
   #$line=$line.'"date": "new Date('.substr($r[0],0,4).','.(((int)substr($r[0],5,2))-1).','.substr($r[0],8,2).','.substr($r[0],11,2).','.substr($r[0],14,2).','.substr($r[0],17,2).'), ';
   #$line=$line.'"date": "'.$r[0].'", ';
-  $line=$line.'"sensors": [';
-  $line=$line.'{"name":"aussen","temperature":'.$r[1].',"humidity":'.$r[2].'}, ';
-  $line=$line.'{"name":"innen","temperature":'.$r[3].',"humidity":'.$r[4].'}, ';
-  $line=$line.'{"name":"eins","temperature":'.$r[5].',"humidity":'.$r[6].'}, ';
-  $line=$line.'{"name":"zwei","temperature":'.$r[7].',"humidity":'.$r[8].'} ';
-  $line=$line.']';	
-  $line=$line.'}';
-  echo ($line);
+  #$line=$line.'"sensors": [';
+  #$line=$line.'{"name":"aussen","temperature":'.$r[1].',"humidity":'.$r[2].'}, ';
+  #$line=$line.'{"name":"innen","temperature":'.$r[3].',"humidity":'.$r[4].'}, ';
+  #$line=$line.'{"name":"eins","temperature":'.$r[5].',"humidity":'.$r[6].'}, ';
+  #$line=$line.'{"name":"zwei","temperature":'.$r[7].',"humidity":'.$r[8].'} ';
+  #$line=$line.']';	
+  #$line=$line.'}';
+  #echo ($line);
 }
+echo json_encode(array('querystringDuration'=>$_GET['duration'],
+		       'duration'=>$duration,
+		       'data:' => $data));
 ?>
-]
-}
+
+
 
 <?php
 
