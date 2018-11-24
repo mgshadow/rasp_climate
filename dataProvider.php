a8a8ee457ba33f49db72919a885485bc8493e233
mgshadow <39982543+mgshadow@users.noreply.github.com>
2018-11-24 14:49:57 +0100

    Update dataProvider.php

--- dataProvider.php
+++ dataProvider.php
@@ -3,7 +3,7 @@ include("include/sensorclass.php");
 $db = mysqli_connect("localhost","datalogger","datalogger") or die("DB Connect error"); 
 mysqli_select_db($db, "datalogger");
 $sql  = 'SELECT min(m.date_time), round(avg(t0.temperature),1) as tt0 , round(avg(t1.temperature),1) as tt1 , round(avg(t2.temperature),1) as tt2 , round(avg(t3.temperature),1) as tt3 FROM measure m left join datalogger as t0 on t0.sensor=0 and t0.measureid=m.id left join datalogger as t1 on t1.sensor=1 and t1.measureid=m.id left join datalogger as t2 on t2.sensor=2 and t2.measureid=m.id left join datalogger as t3 on t3.sensor=3 and t3.measureid=m.id WHERE m.active=1 and TIMESTAMPDIFF(HOUR,m.date_time,NOW())<30 GROUP BY m.date_time order by m.date_time';
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($db, $sql);
 
 ?>
 {
